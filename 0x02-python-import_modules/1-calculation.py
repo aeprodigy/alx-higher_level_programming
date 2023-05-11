7961d53f0fa9260cd7e94cad868151ee435c99d2
mike mambwe <mikeapple056@gmail.com>
2023-05-11 00:23:14 -0700

    calculations

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    num1 = 10
+    num2 = 5
+    print("{} + {} = {}".format(num1, num2, add(num1, num2)))
+    print("{} - {} = {}".format(num1, num2, sub(num1, num2)))
+    print("{} * {} = {}".format(num1, num2, mul(num1, num2)))
+    print("{} / {} = {}".format(num1, num2, div(num1, num2)))
