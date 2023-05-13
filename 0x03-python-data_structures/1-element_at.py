9267156440a576f045462587f5442747ecb15399
mike mambwe <mikeapple056@gmail.com>
2023-05-12 23:57:58 -0700

    elements

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def element_at(my_list, jdx):
+    if jdx < 0:
+        return (None)
+
+    length = len(my_list)
+
+    if jdx > length - 1:
+        return (None)
+
+    return(my_list[jdx])i
